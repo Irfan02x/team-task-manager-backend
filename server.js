b595de8617f21f1890fad217039993261c12de6f
Irfan <151149459+Irfan02x@users.noreply.github.com>
2026-05-02 22:30:59 +0530

    fix cors for frontend

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT =process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-  origin: true,
+  origin: "https://team-task-manager-frontend-production-cd8e.up.railway.app",
   credentials: true
 }));
 app.use(express.json());
